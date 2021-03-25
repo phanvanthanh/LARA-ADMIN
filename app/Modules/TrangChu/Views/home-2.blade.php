dab9f1690c8c2ebedcd34d6222eaff5246a0a73a
Thanh <thanhpv.tvh@vnpt.vn>
2021-03-25 09:41:13 +0700

    Phan Văn Thanh - Add home Views/home-2.blade.php

---
+++ app/Modules/TrangChu/Views/home-2.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.home-layout')
+@section('title', 'Trang chủ')
+@section('content')
+	<div class="row" style="margin-top: 10px;">
+		<div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		 	<div class="blog-slider">
+			  <div class="blog-slider__wrp swiper-wrapper">
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/jason-leung-798979-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor2</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/alessandro-capuzzi-799180-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    
+			  </div>
+			  <div class="blog-slider__pagination"></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row" style="margin-top: 10px;">
+		<div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		 	<div class="blog-slider">
+			  <div class="blog-slider__wrp swiper-wrapper">
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/jason-leung-798979-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor2</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/alessandro-capuzzi-799180-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    
+			  </div>
+			  <div class="blog-slider__pagination"></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row" style="margin-top: 10px;">
+		<div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		 	<div class="blog-slider">
+			  <div class="blog-slider__wrp swiper-wrapper">
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/jason-leung-798979-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor2</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    
+			    <div class="blog-slider__item swiper-slide">
+			      <div class="blog-slider__img">
+			        <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/alessandro-capuzzi-799180-unsplash.jpg" alt="">
+			      </div>
+			      <div class="blog-slider__content">
+			        <span class="blog-slider__code">26 December 2019</span>
+			        <div class="blog-slider__title">Lorem Ipsum Dolor</div>
+			        <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
+			        <a href="#" class="blog-slider__button">READ MORE</a>
+			      </div>
+			    </div>
+			    
+			  </div>
+			  <div class="blog-slider__pagination"></div>
+			</div>
+		</div>
+	</div>
+@endsection
+
+
